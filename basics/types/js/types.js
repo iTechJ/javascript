3725ebb3b454d8e11292287c358506ac2102de01
AntonNekrasov <Anton.Nekrasov@wsa-212.icx.local>
2015-02-09 18:36:57 +0300

    added styling to types

--- basics/types/js/types.js
+++ basics/types/js/types.js
@@ -83,20 +83,28 @@ document.writeln("<br>");
 
 //Null & Undefined
 document.writeln("<b>NULL & UNDEFINED</b>");
-document.writeln(null == undefined);
-
-document.writeln(null > 0);
-document.writeln(null == 0);
-document.writeln(null >= 0);
-
-document.writeln(undefined > 0);
-document.writeln(undefined == 0);
-document.writeln(undefined >= 0);
+document.writeln("<br>");
+document.writeln("null == undefined is " + (null == undefined));
+document.writeln("<br>");
+document.writeln("null > 0 is " + (null > 0));
+document.writeln("<br>");
+document.writeln("null == 0 is " + (null == 0));
+document.writeln("<br>");
+document.writeln("null >= 0 is " + (null >= 0));
+document.writeln("<br>");
+document.writeln("undefined > 0 is " + (undefined > 0));
+document.writeln("<br>");
+document.writeln("undefined == 0 is " + (undefined == 0));
+document.writeln("<br>");
+document.writeln("undefined >= 0 is " + (undefined >= 0));
+document.writeln("<br>");
+document.writeln("<br>");
 
 //Object
-document.writeln(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> OBJECT");
+document.writeln("<b>OBJECT</b>");
+document.writeln("<br>");
 
-obj1 = Object.create(Object.prototype, {
+var obj1 = Object.create(Object.prototype, {
     key1 : {
         writable: true,
         configurable: true,
@@ -109,48 +117,115 @@ obj1 = Object.create(Object.prototype, {
     }
 });
 
-obj2 = {
+var obj2 = {
     key1: "val1",
     key2: "val2"
 };
 
-document.writeln(obj2);
-document.writeln(typeof obj1);
-document.writeln(obj1.key1);
-document.writeln(obj1["key1"]);
+document.writeln("obj1 = " + JSON.stringify(obj1) );
+document.writeln("<br>");
+document.writeln("obj2 = " + JSON.stringify(obj2) );
+document.writeln("<br>");
+document.writeln("<br>");
+
+document.writeln("typeof obj1 is " + typeof obj1);
+document.writeln("<br>");
+document.writeln("obj1.key1 is " + obj1.key1);
+document.writeln("<br>");
+document.writeln("obj1[\"key1\"] is " + obj1["key1"]);
+document.writeln("<br>");
 
 obj1.key2 = "new";
-document.writeln(obj1.key2);
+
+document.writeln("<br>");
+document.writeln("obj1.key2 = \"new\";");
+document.writeln("<br>");
+document.writeln("obj1.key2 is " + obj1.key2);
+document.writeln("<br>");
+document.writeln("<br>");
 
 //Array
-document.writeln(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ARRAY");
+document.writeln("<b>ARRAY</b>");
+document.writeln("<br>");
 
 var arr1 = ["one", "two"];
-//var arr2 = new Array();
-document.writeln(arr1);
-document.writeln(typeof arr1);
+var arr2 = new Array();
+
+document.writeln("var arr1 = [\"one\", \"two\"];");
+document.writeln("var arr2 = new Array();");
+document.writeln("<br>");
+document.writeln("<br>");
+
+document.writeln("arr1 is " + arr1);
+document.writeln("<br>");
+document.writeln("typeof arr1 is " + typeof arr1);
+document.writeln("<br>");
+
 arr1.push("three");
-document.writeln(arr1);
+
+document.writeln("arr1.push(\"three\");")
+document.writeln("<br>");
+document.writeln("arr1 is " + arr1);
+document.writeln("<br>");
+document.writeln("<br>");
 
 //Function
-document.writeln(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> FUNCTION");
+document.writeln("<b>FUNCTION</b>");
+document.writeln("<br>");
 var func = function(){
     document.writeln("ONE");
 };
-document.writeln(typeof func());
+document.writeln("var func = function(){");
+document.writeln("<br>&nbsp;&nbsp;&nbsp;&nbsp; document.writeln(\"ONE\");");
+document.writeln("<br>};");
+document.writeln("<br>");
+document.writeln("<br>");
+
+document.writeln("typeof func() is " + (typeof func));
+document.writeln("<br>");
+document.writeln("func(): ");
 func();
+document.writeln("<br>");
+document.writeln("<br>");
 
 var defFunc = function() {
-    return function() {document.writeln("TWO");}
+    return function() {
+        document.writeln("TWO");
+    }
 };
+
+document.writeln("var defFunc = function() {");
+document.writeln("<br>&nbsp;&nbsp;&nbsp;&nbsp;return function() {");
+document.writeln("<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;document.writeln(\"TWO\");");
+
+document.writeln("<br>");
+document.writeln("<br>");
+document.writeln("defFunc()(): ");
+
 defFunc()();
 
+document.writeln("<br>");
+document.writeln("<br>");
+
+
 //Regular expressions
-document.writeln(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> REGEXP");
-document.writeln( /\s/.test("some string"));
+document.writeln("<b>REGEXP</b>");
+document.writeln("<br>");
+document.writeln("/\\s/.test(\"some string\") is " + (/\s/.test("some string")));
+document.writeln("<br>");
+document.writeln("<br>");
 
 //Date
-document.writeln(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>> DATE");
+document.writeln("<b></b>DATE");
+document.writeln("<br>");
+
 var dt = new Date();
-document.writeln(dt);
-document.writeln(dt.getFullYear());
+
+document.writeln("var dt = new Date();");
+document.writeln("<br>");
+document.writeln("<br>");
+document.writeln("dt is " + dt);
+document.writeln("<br>");
+document.writeln("dt.getFullYear() is " + dt.getFullYear());
+document.writeln("<br>");
+document.writeln("<br>");
