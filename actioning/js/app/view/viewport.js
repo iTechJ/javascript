3539becdc3e49e1218c859b79586487e1765829a
AntonNekrasov <surikat85@gmail.com>
2015-03-22 23:16:46 +0300

    todo bits

--- actioning/js/app/view/viewport.js
+++ actioning/js/app/view/viewport.js
@@ -1,16 +1,21 @@
 /* jshint strict: true */
 /* globals rpApp */
 
-rpApp.Viewport = function() {
+rpApp.Viewport = function(views) {
     "use strict";
-    
+    this.views = views ? views : [];
 };
 
 rpApp.extend(rpApp.Viewport, rpApp.BaseComponent);
 
 rpApp.Viewport.prototype.render = function() {
     "use strict";
+    var container = document.createElement("DIV");
     
-    
+    /*
+        TODO: iterate through the views and add them to the container
+        each element is configured, and should be rendered based on this configuration.
+    */
+
     // return Fish.superclass.toString.call(this) + ", " + this.esculent? "esculent": "not esculent";
 };
